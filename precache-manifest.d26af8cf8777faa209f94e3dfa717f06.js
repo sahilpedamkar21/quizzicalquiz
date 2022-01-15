740a223dc30224d8f405eb16471cb3aab3d11bee
sahilpedamkar21 <sahilpedamkar18@gmail.com>
2022-01-15 18:27:41 +0530

    Updates

---
+++ precache-manifest.d26af8cf8777faa209f94e3dfa717f06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f04eff76256e0dc17f9888dc9a3b43b5",
+    "url": "/quizzicalquiz/index.html"
+  },
+  {
+    "revision": "2e87968c90a53226aee0",
+    "url": "/quizzicalquiz/static/css/main.e1883257.chunk.css"
+  },
+  {
+    "revision": "9f8f410e86b220aa4f9e",
+    "url": "/quizzicalquiz/static/js/2.a7f256fe.chunk.js"
+  },
+  {
+    "revision": "2e87968c90a53226aee0",
+    "url": "/quizzicalquiz/static/js/main.02ea0acc.chunk.js"
+  },
+  {
+    "revision": "baf805dca67ff5a11176",
+    "url": "/quizzicalquiz/static/js/runtime~main.e9c9495f.js"
+  }
+]);
